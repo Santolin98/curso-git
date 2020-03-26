91846877bd2f00382b94b84b2a5e7a21ea7c8154
Gabriel Santolin <gabrielsantolin15@gmail.com>
2020-03-26 11:00:52 -0300

    Projeto iniciado, mostrando a mensagem

---
+++ ProjetodeTesteGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetodeTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste do Git");
+            Console.ReadLine();
+        }
+    }
+}
